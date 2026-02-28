4999237c9d1cc3e142d3a0c33727dfd7d09cc3f7
Nath2504 <2475921@cegepmontpetit.ca>
2026-02-28 12:30:19 -0500

    ajout méthode selectType

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,21 +1,25 @@
 export default class Poutine {
   constructor(element) {
     this.element = this.element;
-    this.poutines = document.querySelector('.js-poutine');
+    this.poutines = this.element.querySelectorAll('.js-poutine');
     this.selectedType = '';
     this.init();
   }
 
   init() {
     for (let i = 0; i < this.poutines.length; i++) {
-      const poutine = poutines[i];
-      this.poutines.addEventListener('click', this.selectedType.bind(this));
+      const button = this.poutines[i];
+      button.addEventListener('click', this.selectedType.bind(this));
     }
   }
 
-  selectType() {
+  selectType(event) {
     for (let i = 0; i < this.poutines.length; i++) {
       this.poutines[i].classList.remove('is-active');
     }
+    const buttonClicked = event.currentTarget;
+    buttonClicked.classList.add('is-active');
+    this.selectedType = buttonClicked.innerText;
+    this.updatePhoto();
   }
 }
